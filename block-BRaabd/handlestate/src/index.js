d5eadf11badbf9b9341188abebac9d6b8ec8f638
aditya <agoswami0708g@gmail.com>
2022-08-20 17:46:17 +0530

    done

--- block-BRaabd/handlestate/src/index.js
+++ block-BRaabd/handlestate/src/index.js
@@ -22,10 +22,12 @@ class Card extends React.Component {
             <button onClick={this.handleClick}>cricket</button>
             <button onClick={this.handleClick}>laptop</button>
             <button onClick={this.handleClick}>phone</button>
+            <button onClick={this.handleClick}>pubg</button>
+            <button onClick={this.handleClick}>show-image</button>
             <button onClick={this.handleClick}>tiger</button>
         </div>
         <div>
-            <img src={`/assets/${this.state.imageName}.jpg`}  width = {'300 px'}/>
+            <img src={`/assets/${this.state.imageName}.jpg`}  width = {'500 px'}/>
         </div>
         </>
     );

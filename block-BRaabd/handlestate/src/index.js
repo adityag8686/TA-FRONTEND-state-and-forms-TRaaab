aa1f4a8ad9444a0320bb6892910cddba4fb2fb5c
aditya <agoswami0708g@gmail.com>
2022-08-20 17:41:11 +0530

    done

---
+++ block-BRaabd/handlestate/src/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+
+class Card extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            imageName : "Basketball",
+        };
+    }
+    handleClick = (event) => {
+        let imagename = event.target.innerText;
+        this.setState({
+            imageName : imagename
+        });
+    };
+    render() {
+        return (
+            <>
+        <div>
+            <button onClick={this.handleClick}>basketball</button>
+            <button onClick={this.handleClick}>cricket</button>
+            <button onClick={this.handleClick}>laptop</button>
+            <button onClick={this.handleClick}>phone</button>
+            <button onClick={this.handleClick}>tiger</button>
+        </div>
+        <div>
+            <img src={`/assets/${this.state.imageName}.jpg`}  width = {'300 px'}/>
+        </div>
+        </>
+    );
+    }
+}
+
+ReactDOM.render(<Card />, document.querySelector("#root"));
